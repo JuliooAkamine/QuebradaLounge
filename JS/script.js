15eeb0402abe7b3a1cc6d39f6741ba24872b79a7
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2024-03-06 17:31:45 -0300

    commit

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 /* Função que quando clica aparece o menu lateral */
 function openNav() {
-    document.getElementById("sidenav-overlay").style.width = "90%"
+    document.getElementById("sidenav-overlay").style.width = "85%"
     document.getElementById("fechar-menu").style.width = "100%"
+    document.body.style.overflow = "hidden"
 }
 
 // função para desaparecer o menu lateral
 function closeNav() {
     document.getElementById("sidenav-overlay").style.width = "0"
     document.getElementById("fechar-menu").style.width = "0"
+    document.body.style.overflow = "scroll"
 }
