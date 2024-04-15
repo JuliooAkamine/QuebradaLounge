b486295a9c706aa42f7cfdb9bf2ecebbc2fa6b10
Julio <julio.caleite@senacsp.edu.br>
2024-04-15 16:56:51 -0300

    Update funcionalidade carrinho

--- JS/adicionarAoCarrinho.js
+++ JS/adicionarAoCarrinho.js
@@ -1,13 +1,16 @@
-    //Adiciona todos os botões de retire na loja ao javascript e adiciona um evento de click a cada um que executa a função addProductToCart
-    
+        
+      
+       function addProductToCart(event) {
+      
+        
 
-    function addProductToCart(event) {
         // Encontra o card do produto associado ao botão que foi clicado
         var cardProduto = event.target.closest('.card-produto');
+
         
         // Encontra a imagem dentro desse card de produto
-        var imagem = cardProduto.querySelector('.product-image');
-        console.log(imagem.src);  // Exibirá o caminho da imagem no console
+        var imagem = cardProduto.querySelector('.product-image').src;
+        console.log(imagem);  // Exibirá o caminho da imagem no console
         
         // Encontra o título dentro desse card de produto
         var productTitle = cardProduto.querySelector('.product-title');
@@ -16,4 +19,27 @@
         var productPrice = cardProduto.querySelector('.product-price');
         console.log(productPrice.textContent)
 
+        
+        
+        /* let produtos = [
+            {
+                nome_produto: productTitle.textContent,  // nome_produto é agora uma string do título do produto
+                preco_produto: productPrice.textContent,  // preco_produto é agora uma string do preço do produto
+                imagem_produto: imagem  // imagem_produto é agora uma string da URL da imagem
+            }
+        ];
+
+        console.log(produtos)
+        */
     }
+  
+
+
+   
+
+
+
+
+
+
+    
