b18752460f7a927cf764b6357883bbdc24c49867
Julio <julio.caleite@senacsp.edu.br>
2024-04-16 17:10:34 -0300

    update do javascript carrinho e alterações nas classes do destaque

--- JS/adicionarAoCarrinho.js
+++ JS/adicionarAoCarrinho.js
@@ -1,38 +1,60 @@
+    //Função para pegar as informações dos produtos e armazena-las
+   function addProductToCart(event) {
+    var cardProduto = event.target.closest('.card-produto');
+    var imagem = cardProduto.querySelector('.product-image').src;
+    var productTitle = cardProduto.querySelector('.product-title').textContent;
+    var productPrice = cardProduto.querySelector('.product-price').textContent;
+
+    var productDetails = {
+        title: productTitle,
+        image: imagem,
+        price: productPrice
+    };
+
+    localStorage.setItem('productDetails', JSON.stringify(productDetails));
+
+}
+
+
+    //Função para resgatar as informações guardadas e converte
+    document.addEventListener('DOMContentLoaded', function() {
+    // Verificar se existem detalhes do produto armazenados no localStorage
+    var productDetails = localStorage.getItem('productDetails');
+    
+    if (productDetails) {
+        // Converter de volta para um objeto JavaScript
+        productDetails = JSON.parse(productDetails);
+        
+        // Acessar os dados e imprimem ele no console
+        console.log('Título do Produto:', productDetails.title);
+        console.log('Imagem do Produto:', productDetails.image);
+        console.log('Preço do Produto:', productDetails.price);
+
+        // Por exemplo, você pode usar esses dados para atualizar o conteúdo da página
+
+        
+        console.log(productDetails.title)
+
+        var imagem = document.getElementById('image-carrinho')
+        var titulo = document.querySelector('.product-tittle-carrinho')
+        var preco = document.querySelector('.product-price-carrinho')
         
       
-       function addProductToCart(event) {
-      
-        
 
-        // Encontra o card do produto associado ao botão que foi clicado
-        var cardProduto = event.target.closest('.card-produto');
+       imagem.src = productDetails.image
+       titulo.innerHTML = productDetails.title
+       preco.innerHTML = productDetails.price
 
-        
-        // Encontra a imagem dentro desse card de produto
-        var imagem = cardProduto.querySelector('.product-image').src;
-        console.log(imagem);  // Exibirá o caminho da imagem no console
-        
-        // Encontra o título dentro desse card de produto
-        var productTitle = cardProduto.querySelector('.product-title');
-        console.log(productTitle.textContent);  // Exibirá o texto do título no console
 
-        var productPrice = cardProduto.querySelector('.product-price');
-        console.log(productPrice.textContent)
 
-        
-        
-        /* let produtos = [
-            {
-                nome_produto: productTitle.textContent,  // nome_produto é agora uma string do título do produto
-                preco_produto: productPrice.textContent,  // preco_produto é agora uma string do preço do produto
-                imagem_produto: imagem  // imagem_produto é agora uma string da URL da imagem
-            }
-        ];
-
-        console.log(produtos)
-        */
     }
-  
+
+});
+
+
+
+    
+ 
 
 
    
