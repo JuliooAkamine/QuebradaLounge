538709d99b307ab6ac8e4a5b85ae30eed4851f16
JuliooAkamine <julio.caleite@senacsp.edu.br>
2024-05-06 14:37:32 -0300

    começando a fazer a autenticação de usuario

---
+++ JS/autenticaçãoUsuario.js
@@ -0,0 +1,7 @@
+function autenticacaologin(event){
+    event.preventDefault()
+    let email = document.getElementById('email-input-entrar').value
+    let password = document.getElementById('loginpassword').value
+
+    alert(email)
+}
